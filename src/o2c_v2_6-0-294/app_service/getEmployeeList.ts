fe45de114ea4da755622366843669a1fb25cce8b
Aman Personal <aman25hussain@gmail.com>
2025-11-28 19:36:26 +0530

    Moving the codebase

---
+++ src/o2c_v2_6-0-294/app_service/getEmployeeList.ts
@@ -0,0 +1,8 @@
+import  { KloAppService } from "kloBo/KloAppService";
+
+export class getEmployeeList extends KloAppService {
+
+    public async onExecute(){
+        // Write your code here
+    }
+}
